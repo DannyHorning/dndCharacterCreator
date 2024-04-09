b75bd80e5e8fa4aa7b9e24673503d70ce74aac5a
Danny Horning <123598793+DannyHorning@users.noreply.github.com>
2024-04-09 17:32:48 -0600

    build bug fixing

--- app/page.js
+++ app/page.js
@@ -9,9 +9,11 @@ export default function Page() {
       <div className='mt-7'>
       <Welcome />
       <p className= 'mt-2 ml-20 mr-20'>
+        {`
             Before we get started on creating your character, let's go over some basic rules of the game. These are fundamental rules that you will need to know in order to play. Understanding these rules
             will help you to make informed decisions when creating your character and when playing the game. There are many different play styles and ways to play DnD, but these rules are the foundation of the game.
             Select a rule from the dropdown menu below to learn more about it.
+            `}
         </p>
       </div>
       <div>
